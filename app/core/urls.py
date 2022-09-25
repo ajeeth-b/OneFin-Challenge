4c01755aea9817bc81bdd8ce4090a1a0b5d2ef5e
ajeethb <ajeethb@caresoftglobal.com>
2022-09-25 23:18:49 +0530

    updated

--- app/core/urls.py
+++ app/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("movies/", views.get_movies),
-    path("collection/", views.CollectionsListAPI.as_view()),
-    path("collection/<uuid:collection_id>/", views.CollectionAPI.as_view()),
+    path("movies/", views.get_movies, name="get_movies",),
+    path("collection/", views.CollectionsListAPI.as_view(), name="collections",),
+    path("collection/<uuid:collection_id>/", views.CollectionAPI.as_view(), name="collection",),
 ]
